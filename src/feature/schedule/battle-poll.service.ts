a94968685245e98c34295b302ba11b8ac63291bf
gavin-shaw <gavin@gnarus.io>
2022-03-14 12:45:08 +0200

    feat: fetch last 3 days of data during team guide

--- src/feature/schedule/battle-poll.service.ts
+++ src/feature/schedule/battle-poll.service.ts
@@ -5,7 +5,7 @@ import { ApiService } from '../../shared/api/api.service';
 import { BattleRepository } from '../../shared/db/battle/battle.repository';
 import { MapperService } from '../../shared/game';
 
-export const DEFAULT_START_BLOCK = 61900134; // 2022-02-17
+export const DEFAULT_START_BLOCK = 62209454; // 2022-02-28T09:17:42
 
 @Processor(WORKER_QUEUE)
 export class BattlePollService {
