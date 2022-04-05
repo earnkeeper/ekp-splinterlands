bc0d18c298623b16af4e4228f58fb52af779a366
gavin-shaw <gavin@gnarus.io>
2022-04-05 23:00:44 +0200

    chore: prep for deploy

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,34 @@ import * as cluster from 'cluster';
 import { GatewayModule } from './gateway.module';
 import { WorkerModule } from './worker.module';
 
-const bootstrap = async () => {
-  if (cluster.default.isPrimary) {
-    const app = await NestFactory.create(GatewayModule);
-    app.enableShutdownHooks();
-    await app.listen(3001);
-  } else {
-    const app = await NestFactory.create(WorkerModule);
-    app.enableShutdownHooks();
-    await app.init();
-  }
+const gatewayBootstrap = async () => {
+  const app = await NestFactory.create(GatewayModule);
+  app.enableShutdownHooks();
+  await app.listen(3001);
 };
 
-Cluster.register(16, bootstrap);
+const workerBootstrap = async () => {
+  const app = await NestFactory.create(WorkerModule);
+  app.enableShutdownHooks();
+  await app.init();
+};
+
+switch (process.env.PROCESS_TYPE) {
+  case 'GATEWAY':
+    Cluster.register(16, gatewayBootstrap);
+    break;
+  case 'WORKER':
+    Cluster.register(16, workerBootstrap);
+    break;
+  default:
+    const combinedBootstrap = async () => {
+      if (cluster.default.isPrimary) {
+        await gatewayBootstrap();
+      } else {
+        await workerBootstrap();
+      }
+    };
+
+    Cluster.register(16, combinedBootstrap);
+    break;
+}
